8de95edebf7b1272c726826a4bfe22d68b0c1ad6
avelasquezn <angel@xendacentral.com>
2011-11-05 12:33:50 -0500

    Bentou initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bentou::Application.config.secret_token = '013b4cba75380141ec790fc11920b3fd62d8c6114d0ca375e31147de7719db4b802458586e37cd520d345a8fe98508722f33c9eb69b19baa347e8770c0f3026e'
